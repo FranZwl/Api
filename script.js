123a1a0e0ab03c92cf1fbb206d63e174a455f3af
FranZwl <fran_-12@hotmail.com>
2024-09-27 12:43:56 -0300

    Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        const lista = data;
+        const cardsContainer = document.querySelector('.container');
+        lista.forEach((usuario, index) => {
+            const row = document.createElement('tr'); 
+            row.innerHTML = `
+                <th scope="row">${index + 1}</th>
+                <td>${usuario.name}</td>
+                <td>${usuario.username}</td>
+                <td>${usuario.email}</td>
+            `;
+            cardsContainer.appendChild(row);
+        });
+    })
+    .catch(error => console.error('Error al obtener datos:', error));
